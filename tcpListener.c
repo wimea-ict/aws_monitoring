695aef20a006a81577e013d078bd29ced96a305b
Kasumba Robert <robein@ymail.com>
2017-08-08 12:53:42 +0300

    Add files via upload

--- tcpListener.c
+++ tcpListener.c
@@ -6,17 +6,14 @@
 #include <sys/types.h>
 #include <string.h>
 #include "nodes.h" //stores the node parameters
-#include </usr/include/mysql/mysql.h>
-#include </usr/include/mysql/my_global.h>
+#include "config.h" //store the configurations
+#include <mysql.h>
+#include <my_global.h>
  #include <unistd.h>
 #include <stdlib.h>
 
-
 #define AF_NET 2
-#define TEN_M_KEY "TXT=makg2-10m"
-#define TWO_M_KEY "TXT=makg2-2m"
-#define GROUND_KEY "TXT=makg2-gnd"
-#define SINK_KEY "TXT=makg2-sink"
+
 struct sockaddr_in addr,clientAddr;
 struct two_meter_record two_meter_node;
 struct ground_node_record ground_node;
@@ -30,6 +27,10 @@ void process10meterString(char buffer[]);
 void processSinkNodeString(char buffer[]);
 void processGroundNodeString(char buffer[]);
 char *getZuluTime(char *normaltime);//format is hh:mm:ss
+void reset2mNodeStruct();
+void resetGroundNodeStruct();
+void resetSinkNodeStruct();
+void reset10mNodeStruct();
 MYSQL *connect2db();
 
 int main(void){
@@ -44,6 +45,7 @@ int main(void){
     
     if(pid>0){
          printf("pid of the child process is %d \n",pid);
+         //kill the parent process to leave the child as a daemon
         exit(0);
     }
     umask(0);
@@ -64,7 +66,7 @@ int main(void){
     int RCVBUFSIZE =256;
     char buffer[RCVBUFSIZE];
     addr.sin_family =AF_NET;
-    addr.sin_port = htons(10020);
+    addr.sin_port = htons(TCP_LISTENER_PORT_NUMBER);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     float dry_bulb,wet_bulb,dew_point,humidity;
     int bind_status = bind(socketId, (struct sockadrr*) &addr, sizeof(addr));
@@ -134,7 +136,10 @@ int main(void){
                              }
                             sprintf(wind_query,wind_template,ten_meter_node.date_10m,station_Number,getZuluTime(ten_meter_node.time_10m),ten_meter_node.v_a1_10m,ten_meter_node.v_a2_10m);
                             fprintf(fp,"\n%s\n",wind_query);
-                            
+                            if (mysql_query(connection,wind_query)) {
+                                fprintf(fp, "%s\n", mysql_error(connection));
+                             }
+                            reset10mNodeStruct();
                         }
                         else if(strstr(buffer,TWO_M_KEY)!=NULL){
                             process2meterString(buffer);
@@ -176,6 +181,7 @@ int main(void){
                             if (mysql_query(connection,temp_humid_query)) {
                                 fprintf(fp, "%s\n", mysql_error(connection));
                              }
+                            reset2mNodeStruct();
                             }
                         else if(strstr(buffer,GROUND_KEY)!=NULL){
                             processGroundNodeString(buffer);
@@ -204,9 +210,10 @@ int main(void){
                              }
                             sprintf(soil_rain_query,soil_rain_template,ground_node.date_gnd,station_Number,getZuluTime(ground_node.time_gnd),ground_node.v_a2_gnd,ground_node.v_a1_gnd,ground_node.p0_lst60_gnd);
                             fprintf(fp,"\n%s\n",soil_rain_query);
-                            if (mysql_query(connection,temp_humid_query)) {
+                            if (mysql_query(connection,soil_rain_query)) {
                                 fprintf(fp, "%s\n", mysql_error(connection));
                              }
+                            resetGroundNodeStruct();//clear all the values in the structure for the ground node
                             }
                         else if(strstr(buffer,SINK_KEY)!=NULL){
                             processSinkNodeString(buffer);
@@ -232,11 +239,12 @@ int main(void){
                             if (mysql_query(connection,status_query)) {
                                 fprintf(fp, "%s\n", mysql_error(connection));
                              }
-                            sprintf(pressure_query,pressure_template,sink_node.date_sink,station_Number,getZuluTime(sink_node.time_sink),sink_node.up_sink);
+                            sprintf(pressure_query,pressure_template,sink_node.date_sink,station_Number,getZuluTime(sink_node.time_sink),sink_node.p_ms5611_sink);
                              fprintf(fp,"\n%s\n",pressure_query);
                             if (mysql_query(connection,pressure_query)) {
                                 fprintf(fp, "%s\n", mysql_error(connection));
                              }
+                             resetSinkNodeStruct();
                         }
                             else{
                                 
@@ -326,10 +334,10 @@ MYSQL *connect2db(){
    MYSQL_RES *res;
    MYSQL_ROW row;
 
-   char *server = "server address";//server address
-   char *user = "username";//the username
-   char *password = "password"; //the password
-   char *database = "database_name";
+   char *server = SERVER;
+   char *user = DATABASE_USERNAME;
+   char *password = DATABASE_USER_PASSWORD; 
+   char *database = DATABASE_NAME;
    conn = mysql_init(NULL);
 
    /* Connect to database */
@@ -382,7 +390,7 @@ void process10meterString(char buffer[]){
             }else if(strstr(reports[counter],V_MCU_10M_KEY) !=NULL){
 
                 ten_meter_node.v_mcu_10m = findKeyValue(reports[counter],V_MCU_10M_KEY);
-            }else if(strstr(reports[counter],T_10M_KEY) !=NULL && strstr(reports[counter],"TZ")==NULL && strstr(reports[counter],"UTC")==NULL){
+            }else if(strstr(reports[counter],T_10M_KEY) !=NULL && strstr(reports[counter],"TZ")==NULL && strstr(reports[counter],"UTC")==NULL && strstr(reports[counter],"TXT")==NULL){
                 ten_meter_node.t_10m = findKeyValue(reports[counter],T_10M_KEY);
 
             }else if(strstr(reports[counter],V_A1_10M_KEY) !=NULL){
@@ -408,6 +416,9 @@ void process10meterString(char buffer[]){
             }
             else if(strstr(reports[counter],DRP_10M_KEY) !=NULL){
                 ten_meter_node.drp_10m = findKeyValue(reports[counter],DRP_10M_KEY);
+            }else if(strstr(reports[counter],TXT_10M_KEY) !=NULL){
+
+                 ten_meter_node.txt_10m= findKeyValue(reports[counter],TXT_10M_KEY);
             }
             
         } 
@@ -473,7 +484,7 @@ void processSinkNodeString(char buffer[]){
             }else if(strstr(reports[counter], P_MS5611_SINK_KEY) !=NULL){
 
                 sink_node.p_ms5611_sink = findKeyValue(reports[counter],P_MS5611_SINK_KEY);
-            }else if(strstr(reports[counter],T_SINK_KEY) !=NULL && strstr(reports[counter],"TZ")==NULL && strstr(reports[counter],"UTC")==NULL){
+            }else if(strstr(reports[counter],T_SINK_KEY) !=NULL && strstr(reports[counter],"TZ")==NULL && strstr(reports[counter],"UTC")==NULL && strstr(reports[counter],"TTL")==NULL){
                 sink_node.t_sink = findKeyValue(reports[counter],T_SINK_KEY);
 
             }else if(strstr(reports[counter],V_IN_SINK_KEY) !=NULL){
@@ -538,10 +549,8 @@ void processGroundNodeString(char buffer[]){
                 ground_node.p0_gnd = findKeyValue(reports[counter],P0_GND_KEY);  
             }else if(strstr(reports[counter],UP_GND_KEY) !=NULL){
                 ground_node.up_gnd = findKeyValue(reports[counter],UP_GND_KEY);
-
             }
             else if(strstr(reports[counter],V_MCU_GND_KEY) !=NULL){
-
                 ground_node.v_mcu_gnd = findKeyValue(reports[counter],V_MCU_GND_KEY);
               }
             else if(strstr(reports[counter],E64_GND_KEY) !=NULL){
@@ -603,6 +612,7 @@ char *findKeyValue(char *substr,char *key){
         return NULL;//the key is not contained in the substr
     }
 }
+
 char *getZuluTime(char *normaltime){//format is hh:mm:ss
     char delim[] =":";
     char *token = (char *) malloc(4);
@@ -623,4 +633,84 @@ char *getZuluTime(char *normaltime){//format is hh:mm:ss
     }
     //free(zulutime);
     return zulutime;  
+}
+
+void reset2mNodeStruct(){
+    two_meter_node.date_2m="";
+    two_meter_node.drp_2m="";
+    two_meter_node.e64_2m="";
+    two_meter_node.gw_lat_2m="";
+    two_meter_node.gw_long_2m="";
+    two_meter_node.lqi_2m="";
+    two_meter_node.rh_sht2x_2m="";
+    two_meter_node.rssi_2m="";
+    two_meter_node.t_sht2x_2m="";
+    two_meter_node.time_2m="";
+    two_meter_node.ttl_2m="";
+    two_meter_node.txt_2m="";
+    two_meter_node.ut_2m="";
+    two_meter_node.v_in_2m="";
+    two_meter_node.v_mcu_2m="";
+}
+
+void resetGroundNodeStruct(){
+    ground_node.date_gnd="";
+    ground_node.drp_gnd="";
+    ground_node.e64_gnd="";
+    ground_node.gw_lat_gnd="";
+    ground_node.gw_long_gnd="";
+    ground_node.lqi_gnd="";
+    ground_node.p0_gnd="";
+    ground_node.p0_lst60_gnd="";
+    ground_node.ps_gnd="";
+    ground_node.rssi_gnd="";
+    ground_node.time_gnd="";
+    ground_node.ttl_gnd="";
+    ground_node.txt_gnd="";
+    ground_node.up_gnd="";
+    ground_node.ut_gnd="";
+    ground_node.v_a1_gnd="";
+    ground_node.v_a2_gnd="";
+    ground_node.v_in_gnd="";
+    ground_node.v_mcu_gnd="";
+}
+
+void resetSinkNodeStruct(){
+    sink_node.date_sink="";
+    sink_node.drp_sink="";
+    sink_node.e64_sink="";
+    sink_node.gw_lat_sink="";
+    sink_node.gw_long_sink="";
+    sink_node.lqi_sink="";
+    sink_node.p_ms5611_sink="";
+    sink_node.ps_sink="";
+    sink_node.rssi_sink="";
+    sink_node.t_sink="";
+    sink_node.time_sink="";
+    sink_node.ttl_sink="";
+    sink_node.txt_sink="";
+    sink_node.up_sink="";
+    sink_node.ut_sink="";
+    sink_node.v_in_sink="";
+    sink_node.v_mcu_sink="";
+}
+
+void reset10mNodeStruct(){
+    ten_meter_node.date_10m="";
+    ten_meter_node.drp_10m="";
+    ten_meter_node.e64_10m="";
+    ten_meter_node.gw_lat_10m="";
+    ten_meter_node.gw_long_10m="";
+    ten_meter_node.lqi_10m="";
+    ten_meter_node.ps_10m="";
+    ten_meter_node.rssi_10m="";
+    ten_meter_node.t_10m="";
+    ten_meter_node.time_10m="";
+    ten_meter_node.ttl_10m="";
+    ten_meter_node.ut_10m="";
+    ten_meter_node.v_a2_10m="";
+    ten_meter_node.v_a3_10m="";
+    ten_meter_node.v_in_10m="";
+    ten_meter_node.v_mcu_10m="";
+    ten_meter_node.txt_10m="";
 }
