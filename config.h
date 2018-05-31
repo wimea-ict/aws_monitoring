86849d919a7cf644d00d60568b969f7431776952
Kasumba Robert <robein@ymail.com>
2018-05-31 09:21:14 +0300

    Upadate

---
+++ config.h
@@ -0,0 +1,9 @@
+#define DATABASE_USERNAME "jmuhumuza"
+#define DATABASE_USER_PASSWORD "joshua"
+#define DATABASE_NAME "wdrDb"
+
+#define SERVER "localhost"
+
+
+#define TCP_LISTENER_PORT_NUMBER 10020
+
